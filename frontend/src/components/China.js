e99381179a32123ce48f2f859f8b630d07e66364
DVK02 <65472342+DVK02@users.noreply.github.com>
2023-01-22 09:05:43 -0500

    updated china and notes page

--- frontend/src/components/China.js
+++ frontend/src/components/China.js
@@ -1,11 +1,53 @@
-import React from "react"
+import React from "react";
 
 function China() {
-    return (
+  return (
     <div>
-        <h1>This is China</h1>
+      <h1>Chinese Schools</h1>
+      <h2>University of Beijing</h2>
+      <h3>Courses</h3>
+      <h4>BIOA01</h4>
+      <table>
+        <tr>
+          <td style={{ fontWeight: "bold" }}>
+            Where are white blood cells located?
+          </td>
+          <td>
+            Your white blood cells are in your bloodstream and travel through
+            blood vessel walls and tissues to locate the site of an infection.
+          </td>
+        </tr>
+        <tr>
+          <td style={{ fontWeight: "bold" }}>How big are white blood cells?</td>
+          <td>
+            You can only see white blood cells under a microscope, as they are
+            extremely small.
+          </td>
+        </tr>
+        <tr>
+          <td style={{ fontWeight: "bold" }}>
+            How are white blood cells formed?
+          </td>
+          <td>
+            White blood cell formation occurs in the soft tissue inside of your
+            bones (bone marrow). Two types of white blood cells (lymphocytes)
+            grow in the thymus gland (T cells) and lymph nodes and spleen (B
+            cells).
+          </td>
+        </tr>
+        <tr>
+          <td style={{ fontWeight: "bold" }}>
+            What are white blood cells made of?
+          </td>
+          <td>
+            White blood cells originate from cells that morph into other cells
+            in the body (stem cell) within the soft tissue of your bones (bone
+            marrow).
+          </td>
+        </tr>
+      </table>
     </div>
-    );
+  );
 }
 
 export default China;
