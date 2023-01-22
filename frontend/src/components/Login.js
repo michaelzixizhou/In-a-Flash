08cfeb257943e912bb87d2724c2f69156f3e668c
michaelzixizhou <106829824+michaelzixizhou@users.noreply.github.com>
2023-01-22 08:39:46 -0500

    login CSS

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,8 +2,56 @@ import React, { useEffect, useState } from "react";
 import styled from 'styled-components';
 
 const LoginWrapper = styled.div`
-  border: 1px;
+  border-radius: 10px;
+  border: 2px solid #000;
+  background-color: #fff;
+  height:500px;
+  position: fixed;
+  display: row;
+  width: 400px;
 `
+
+const FormWrapper = styled.div`
+  position: relative;
+  vertical-align: top;
+`
+const LoginHeader = styled.div`
+  font-size: 30px;
+  margin-top: 50px;
+  margin-bottom: 100px;
+  color: #4169e1;
+  font-family: sans-serif;
+`
+const Button = styled.button`
+  background-color: orange; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin-top: 60px;
+  border-radius: 10px;
+`
+const Label = styled.label`
+  padding: 10px;
+  font-size: 20px;
+  
+`
+
+const Input = styled.input`
+  :focus {
+    border: 3px solid #555;
+  }
+  width: 200px;
+  margin-bottom: 20px;
+`
+
+const LoginText = styled.div`
+  font-size: 20px;
+`
+
 
 function Login() {
   // state variable to track the login status
@@ -40,17 +88,28 @@ function Login() {
           <button onClick={handleLogout}>Logout</button>
         </div>
       ) : (
-        <form>
-          <label>
-            Email:
-            <input type="email" name="email" required />
-          </label>
-          <label>
-            Password:
-            <input type="password" name="password" required />
-          </label>
-          <button onClick={handleLogin}>Login</button>
-        </form>
+          <FormWrapper>
+            <LoginHeader>Welcome, please sign in.</LoginHeader>
+            <form>
+              <LoginText>Email:</LoginText>
+              <Label style={{
+              }
+              }>
+            
+            <Input type="email" name="email" required />
+              </Label>
+              
+              <div>
+                <LoginText>Password:</LoginText>
+              <Label>
+            <Input type="password" name="password" required />
+              </Label>
+                
+          </div>
+         
+          <Button onClick={handleLogin}>Login</Button>
+            </form>
+          </FormWrapper>
       )}
     </LoginWrapper>
   );
